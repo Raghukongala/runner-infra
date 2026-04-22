b8ee778a0b080ef63e6e891d1e0a74b2f37e9b38
Raghukongala <kongalaraghu406@gmail.com>
2026-04-22 14:47:16 +0530

    Update BACKEND.tf

--- BACKEND.tf
+++ BACKEND.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "raghu-buk"
-    key            = "ecs/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock"
+    bucket = "raghu-buk"
+    key    = "ecs/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
